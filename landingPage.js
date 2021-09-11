4c0eaa2eadc5f0adaf7546b900cb39c34c1f90fa
KevinOrtizSarmiento <kevinsebastianortizsarmiento@gmail.com>
2021-09-10 21:11:52 -0500

    Landing Page + Regsitro Administrador (se haran modificaciones despues)

---
+++ landingPage.js
@@ -0,0 +1,5 @@
+/* Esto servira al momento de hacer el Backend */
+function validarIncioSesion() {
+    const email = document.getElementById("email").value ;
+    const password = document.getElementById("password").value ;
+}
