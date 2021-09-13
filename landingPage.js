adfd61738f970e10985add3c21fc32492410e3b5
KevinOrtizSarmiento <kevinsebastianortizsarmiento@gmail.com>
2021-09-13 14:33:05 -0500

    Codigo sin boostrap organizado en sus respectivas carpetas

--- landingPage.js
+++
@@ -1,5 +0,0 @@
-/* Esto servira al momento de hacer el Backend */
-function validarIncioSesion() {
-    const email = document.getElementById("email").value ;
-    const password = document.getElementById("password").value ;
-}
