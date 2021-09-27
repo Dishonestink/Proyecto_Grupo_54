132afbdb48c0113fcd2f69237c2251ec0f81fd35
Nicolás (Disho) <dishonestink.2001@gmail.com>
2021-09-26 20:19:29 -0500

    Final de primer Vue.js

--- JS/ModificacionEstudiantes.js
+++ JS/ModificacionEstudiantes.js
@@ -1,29 +1,39 @@
-let buscador = new Vue({
-    el:"#buscador",
+let app = new Vue({
+    el:'#divCentralDer',
     data() {
         return {
+            items : [
+                {nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                {nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                {nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                {nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                {nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {nombre: 'Carmen Valenzuela Bernal ',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                ],
             buscador: '', //Estos valores se usará de filtro
             grado:'',
             curso:'',
         }
     },
-})
-
-
-
-let app = new Vue({
-    el:'#app',
-    data() {
-        return {
-            items : [
-                {nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
-                {nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'}
-                ],
-        }
-    },
-    /*methods: {
-        adicionar() {
-
-        }
-    }*/
+    methods: {
+        eliminar(row) {
+            if (confirm("¿Está seguro de eliminar?")){
+                this.items.splice(row,1);}
+        },
+        añadir() {
+            this.items.push({
+                nombre:'',
+                identificacion:'',
+                situacion:'',
+                grado:'',
+            })
+        },
+        guardar() {
+            this.items = this.items;
+        },
+    }
 })
