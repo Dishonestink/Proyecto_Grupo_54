6c1c5796d8f07f621199cb9f8cc1b4de67d7bbfe
KevinOrtizSarmiento <kevinsebastianortizsarmiento@gmail.com>
2021-09-28 23:32:08 -0500

    github

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,10 +2,7 @@
   <div class="hello">
     <button class="btn btn-primary">xxxxxx</button>
     <h1>{{ msg }}</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
-<button type="button" class="btn btn-secondary">Secondary</button>
-<button type="button" class="btn btn-success">Success</button>
-<button type="button" class="btn btn-danger">Danger</button>
+    
 
 <button type="button" class="btn btn-link">Link</button>
     <p>
