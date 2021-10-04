4de6153c19bcda201b3b99cd2d922d881564c09e
Nicolás (Disho) <dishonestink.2001@gmail.com>
2021-10-04 12:59:46 -0500

    Quinto avance de Vue

--- VUE/academica/src/views/ModificacionEstudiantes.vue
+++ VUE/academica/src/views/ModificacionEstudiantes.vue
@@ -284,6 +284,39 @@ export default {
         MainHead,
         Menu,
         Footer,
+    },
+    data() {
+        return {
+            items : [
+                {id:"1",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {id:"2",nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                {id:"3",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {id:"4",nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                {id:"5",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {id:"6",nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                {id:"7",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {id:"8",nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                {id:"9",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
+                {id:"10",nombre: 'Carmen Valenzuela Bernal ',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
+                ],
+            buscador: '', //Estos valores se usará de filtro
+            grado:'',
+            curso:'',
+        }
+    },
+    methods: {
+        eliminar(row) {
+            if (confirm("¿Está seguro de eliminar?")){
+                this.items.splice(row,1);}
+        },
+        añadir() {
+            this.items.push({
+                nombre:'',
+                identificacion:'',
+                situacion:'',
+                grado:'',
+            })
+        },
     }
 }
 </script>
--- VUE/academica/src/views/ModificacionHorariosSalones.vue
+++ VUE/academica/src/views/ModificacionHorariosSalones.vue
@@ -247,6 +247,75 @@ export default {
         MainHead,
         Menu,
         Footer,
-    },   
+    },
+    data() {
+        return {
+            items1 : [
+                {id:"1",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+                {id:"2",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+                {id:"3",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+                {id:"4",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+                {id:"5",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+                {id:"6",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+                {id:"7",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+                {id:"8",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+                {id:"9",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+                {id:"10",nombre:'William Díaz',identificacion:'123127',situacion:'Empleado',asignatura:'Español',grado:'10°,11°',horario:'<a href="https://plantillasdememes.com/img/plantillas/nada-no-hay-no-existe01587466097.jpg">Horario</a>'},
+            ],
+            items2 : [
+                {id:"1",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+                {id:"2",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+                {id:"3",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+                {id:"4",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+                {id:"5",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+                {id:"6",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+                {id:"7",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+                {id:"8",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+                {id:"9",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+                {id:"10",salon:'110',estado:'Ocupado',responsable:'Grado 6°-1',horario:'Lunes - Viernes 6:00 - 11:59 am',},
+            ],
+            buscador: '', //Estos valores se usará de filtro
+            salones: false,
+            horarios: true,
+        }
+    },
+    methods: {
+        eliminar1(row) {
+            if (confirm("¿Está seguro de eliminar?")){
+                this.items1.splice(row,1);
+            }
+        },
+        eliminar2(row) {
+            if(confirm("¿Está seguro de eliminar?")){
+                this.items2.splice(row,1);
+            }
+        },
+        añadir1() {
+            this.items1.push({
+                nombre:'',
+                identificacion:'',
+                situacion:'',
+                asignatura:'',
+                grado:'',
+                horario:'',
+            })
+        },
+        añadir2() {
+            this.items2.push({
+                salon:'',
+                estado:'',
+                responsable:'',
+                horario:'',
+            })
+        },
+        fhorarios(){
+            this.horarios = true;
+            this.salones = false;
+        },
+        fsalones() {
+            this.horarios = false;
+            this.salones = true;
+        },
+    }
 }
 </script>
