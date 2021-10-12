2ae66d34b8d9cbc946ad2a96d01745280e3f96b8
Nicolás (Disho) <dishonestink.2001@gmail.com>
2021-10-12 17:26:55 -0500

    Solución de error de tablas de cursos

--- VUE/academica/src/views/ModificacionEstudiantes.vue
+++ VUE/academica/src/views/ModificacionEstudiantes.vue
@@ -1,47 +1,47 @@
 <template>
-    <div id="divPrincipal">
-        <MainHead/>
-        <div id="divCentral" class="Division">
-            <Menu/>
-            <section id="divCentralDer"> <!--Se identifica una sección central con la etiqueta semántica de SECTION-->
-            <header id="headerDer"> <!--Se añade otra etiqueta semántica de HEADER-->
-                <span><h1 id="HeaderSentence"><b>Modificación de Estudiantes</b></h1></span>
-                
-                <div class="busqueda" id="buscador"> <!--Se restringe un sección para la barra de busqueda y el botón sumbit-->
-                    <h5>Buscador de estudiantes</h5>
-                    <input class="Barra" type="search" name="Lupa" placeholder="¿A quién desea buscar?" v-model="buscador">
-                    <input for="Lupa" class="lupa"  type="submit" value="&#8981;">
-                </div>
-                
-            </header>
-            <!--Fin de Header-->
-            <div id="Métodos"> <!--Sección para los botones que modifican la vista y la tabla-->
-                <div id="Métodos_1" style="flex-grow: 1;"><!--botones de vista-->
-                    <h4 id="EnunFiltro">Filtre por grado</h4>
-                    <select id="grado" v-model="grado">
-                        <option selected>
+  <div id="divPrincipal">
+    <MainHead />
+    <div id="divCentral" class="Division">
+      <Menu />
+      <section id="divCentralDer">
+        <!--Se identifica una sección central con la etiqueta semántica de SECTION-->
+        <header id="headerDer">
+          <!--Se añade otra etiqueta semántica de HEADER-->
+          <span
+            ><h1 id="HeaderSentence">
+              <b>Modificación de Estudiantes</b>
+            </h1></span
+          >
 
-                        </option>
-                        <option>
-                            6°
-                        </option>
-                        <option>
-                            7°
-                        </option>
-                        <option>
-                            8°
-                        </option>
-                        <option>
-                            9°
-                        </option>
-                        <option>
-                            10°
-                        </option>
-                        <option>
-                            11°
-                        </option>
-                    </select>
-                    <select id="curso" v-model="curso">
+          <div class="busqueda" id="buscador">
+            <!--Se restringe un sección para la barra de busqueda y el botón sumbit-->
+            <h5>Buscador de estudiantes</h5>
+            <input
+              class="Barra"
+              type="search"
+              name="Lupa"
+              placeholder="¿A quién desea buscar?"
+              v-model="buscador"
+            />
+            <input for="Lupa" class="lupa" type="submit" value="&#8981;" />
+          </div>
+        </header>
+        <!--Fin de Header-->
+        <div id="Métodos">
+          <!--Sección para los botones que modifican la vista y la tabla-->
+          <div id="Métodos_1" style="flex-grow: 1">
+            <!--botones de vista-->
+            <h4 id="EnunFiltro">Filtre por grado</h4>
+            <select id="grado" v-model="grado">
+              <option selected></option>
+              <option>6°</option>
+              <option>7°</option>
+              <option>8°</option>
+              <option>9°</option>
+              <option>10°</option>
+              <option>11°</option>
+            </select>
+            <!--<select id="curso" v-model="curso">
                         <option selected>
                             
                         </option>
@@ -57,169 +57,186 @@
                         <option>
                             4
                         </option>
-                    </select>
-                </div>
-                <!--<div id="Métodos_oculto">
+                    </select>-->
+          </div>
+          <!--<div id="Métodos_oculto">
                     Sección oculta
                 </div>-->
-                <div id=Métodos_2 style="flex-grow: 1;"> <!--botones de tabla-->
-                    
-                    <button v-on:click="añadir()">Añadir</button>
-                    <!--<button>Eliminar</button>-->
-                    <button v-on:click="guardar()">Guardar</button>
-                </div>
-            </div>
-            <div class="ContenedorTabla"> <!--Seccion que contiene la tabla y la barra de desplazamiento-->
-                <div id="app">
-                    <table id="tablaPrincipal" class="table"> <!--Información de Tabla-->
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Identificación</th>
-                            <th>Situación de estudiante</th>
-                            <th>Grado actual</th>
-                            <th>Eliminar</th>
-                        </tr>
-                        <!--<tr>
+          <div id="Métodos_2" style="flex-grow: 1">
+            <!--botones de tabla-->
+
+            <button v-on:click="añadir()">Añadir</button>
+            <!--<button>Eliminar</button>-->
+            <button v-on:click="guardar()">Guardar</button>
+          </div>
+        </div>
+        <div class="ContenedorTabla">
+          <!--Seccion que contiene la tabla y la barra de desplazamiento-->
+          <div id="app">
+            <table id="tablaPrincipal" class="table">
+              <!--Información de Tabla-->
+              <tr>
+                <th>Nombre</th>
+                <th>Identificación</th>
+                <th>Situación de estudiante</th>
+                <th>Grado actual</th>
+                <th>Eliminar</th>
+              </tr>
+              <!--<tr>
                             <td>Oscar Campos </td>
                             <td>234234 </td>
                             <td>Matriculado </td>
                             <td>6°-1</td>
                         </tr>-->
-                        <tr v-for="(item,index) in items" :key="item.id" contenteditable="true">
-                            <td v-text="item.nombre"></td>
-                            <td v-text="item.identificacion"></td>
-                            <td v-text="item.situacion"></td>
-                            <td v-text="item.grado"></td>
-                            <td id="celdaEliminar"><input id="btnEliminar" type="button" value="&#128465;" v-on:click="eliminar(index)"></td>
-                        </tr>
-                          
-    
-                        <!--Fin información de tabla-->
-                    </table>
-                    
-                </div>
-                <!--Fin de información de tabla-->
-            </div>
-            </section>
-            <!--Fin de la sección SECTION-->
+              <tr
+                v-for="(item, index) in items"
+                :key="item.id"
+                contenteditable="true"
+              >
+                <td v-text="item.nombre"></td>
+                <td v-text="item.identificacion"></td>
+                <td v-text="item.situacion"></td>
+                <td v-text="item.grado"></td>
+                <td id="celdaEliminar">
+                  <input
+                    id="btnEliminar"
+                    type="button"
+                    value="&#128465;"
+                    v-on:click="eliminar(index)"
+                  />
+                </td>
+              </tr>
+
+              <!--Fin información de tabla-->
+            </table>
+          </div>
+          <!--Fin de información de tabla-->
         </div>
-        <Footer/>
+      </section>
+      <!--Fin de la sección SECTION-->
     </div>
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
 * {
-    margin:0px;
+  margin: 0px;
 }
 
 button {
-    cursor:pointer;
+  cursor: pointer;
 }
 
 select {
-    border-radius: 4px ;
-    margin-right: 8px;
-    cursor:pointer;
+  border-radius: 4px;
+  margin-right: 8px;
+  cursor: pointer;
 }
 
 #divPrincipal {
-    background-color: white;
-    display: flex;/*Se especifica que el entorno de la página será de tipo flexible*/
-    align-content: flex-end;/*la alineación horizontal tenderá al final (izquierda)*/
-    align-items: flex-start;/*la alineación vertical tenderá al inicio (parte superior)*/
-    flex-direction: column;/*Se mantendrá un orden de llenado vertical*/
+  background-color: white;
+  display: flex; /*Se especifica que el entorno de la página será de tipo flexible*/
+  align-content: flex-end; /*la alineación horizontal tenderá al final (izquierda)*/
+  align-items: flex-start; /*la alineación vertical tenderá al inicio (parte superior)*/
+  flex-direction: column; /*Se mantendrá un orden de llenado vertical*/
 }
 #divPrincipal > div {
-    width:100%;
+  width: 100%;
 }
 
 /*Se dan las caracteristicas principales de la sección central, que contiene la sección NAV y FIGURE*/
 #divCentral {
-    position:relative;
-    left:0;
-    margin-top: 10px;
-    height:auto;
-    width:100%;
-    border:1px solid black;
+  position: relative;
+  left: 0;
+  margin-top: 10px;
+  height: auto;
+  width: 100%;
+  border: 1px solid black;
 }
 
 /*Se dan las caracteristicas en la sección anterior pero mediante método flexible*/
 .Division {
-    display:flex;
-    flex-wrap: nowrap;
-    justify-content: flex-start;
-    align-items: flex-start;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
 }
 
 /*Caracteristicas de la sección de Section*/
 #divCentralDer {
-    display: flex;
-    flex-direction:column;
-    flex-wrap: nowrap;
-    background-color: white;
-    color: black;
-    width: 80%;
-    height:572px;
-    border:0.5px solid black;
-    margin-bottom: 0px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  background-color: white;
+  color: black;
+  width: 80%;
+  height: 572px;
+  border: 0.5px solid black;
+  margin-bottom: 0px;
 }
 /*Caracteristicas del HEADER derecho*/
 #headerDer {
-    display: flex;
-    flex-wrap: nowrap;
-    text-align: left;
-    padding:10px;
-    margin:20px;
-    margin-bottom:30px;
+  display: flex;
+  flex-wrap: nowrap;
+  text-align: left;
+  padding: 10px;
+  margin: 20px;
+  margin-bottom: 30px;
 }
 
-#HeaderSentence { /*Especifica la distancia entre el header y la sección de botones*/
-    margin-bottom: 40px;
+#HeaderSentence {
+  /*Especifica la distancia entre el header y la sección de botones*/
+  margin-bottom: 40px;
 }
 
 .Barra {
-    height:22px;
-    width:177px;
+  height: 22px;
+  width: 177px;
 }
 
-.busqueda {/*Indica que la sección de busqueda es flexible y debe mantenerse en una linea*/
-    display:flex;
-    align-items:start;
-    justify-content:end;
-    flex-wrap: nowrap;
-    margin:0px;
-    margin-left:200px;
+.busqueda {
+  /*Indica que la sección de busqueda es flexible y debe mantenerse en una linea*/
+  display: flex;
+  align-items: start;
+  justify-content: end;
+  flex-wrap: nowrap;
+  margin: 0px;
+  margin-left: 200px;
 }
 
-.lupa { /*Se gira la posicion del simbolo de lupa*/
-    transform:rotate(-90deg);
-    cursor:pointer;
+.lupa {
+  /*Se gira la posicion del simbolo de lupa*/
+  transform: rotate(-90deg);
+  cursor: pointer;
 }
 
-#Métodos { /*La sección de botones serán flexible, mantenerse alineados verticalmente y 
+#Métodos {
+  /*La sección de botones serán flexible, mantenerse alineados verticalmente y 
     centrados horizontalmente*/
-    display:flex;
-    flex-wrap: nowrap;
-    margin-left:10%;
-    margin-right: 10%;
-    /*align-items:stretch;
+  display: flex;
+  flex-wrap: nowrap;
+  margin-left: 10%;
+  margin-right: 10%;
+  /*align-items:stretch;
     justify-content:baseline;*/
-    width: 80%;
-    margin-bottom: 50px;
+  width: 80%;
+  margin-bottom: 50px;
 }
 
-#Métodos_1 { /*Se alinean al costado izquierdo (inicio)*/
-    display:flex;
-    flex-wrap: nowrap;
-    width: auto;
-    height:auto;
-    margin-right: 0%;
-    align-items:center;
-    justify-content:flex-start;
+#Métodos_1 {
+  /*Se alinean al costado izquierdo (inicio)*/
+  display: flex;
+  flex-wrap: nowrap;
+  width: auto;
+  height: auto;
+  margin-right: 0%;
+  align-items: center;
+  justify-content: flex-start;
 }
 
 #EnunFiltro {
-    margin-right: 50px;
+  margin-right: 50px;
 }
 
 /*#Métodos_oculto {
@@ -228,47 +245,52 @@ select {
     display:hidden;
 }*/
 
-#Métodos_2 {/*Se alinean al costado derecho (fin)*/
-    display:flex;
-    flex-wrap: nowrap;
-    width: auto ;
-    height:auto;
-    margin-left: 0px;
-    align-items: center;
-    justify-content: flex-end;
+#Métodos_2 {
+  /*Se alinean al costado derecho (fin)*/
+  display: flex;
+  flex-wrap: nowrap;
+  width: auto;
+  height: auto;
+  margin-left: 0px;
+  align-items: center;
+  justify-content: flex-end;
 }
 
-.ContenedorTabla {/*Se añade la barra de desplazamiento y se especifica dicha region sumada a la tabla*/
-    overflow-y: auto;
-    width: 80%;
-    margin-left: 10%;
-    margin-right: 10%;
-    max-height: 300px;
-    border:2px solid black;
+.ContenedorTabla {
+  /*Se añade la barra de desplazamiento y se especifica dicha region sumada a la tabla*/
+  overflow-y: auto;
+  width: 80%;
+  margin-left: 10%;
+  margin-right: 10%;
+  max-height: 300px;
+  border: 2px solid black;
 }
 
-table { /*Se acota el área de la tabla dinámica al tamaño de la pantalla*/
-    display:inline-table;
-    width: 100%;
-    max-height:318px; 
-    /*margin-left: 10%;
+table {
+  /*Se acota el área de la tabla dinámica al tamaño de la pantalla*/
+  display: inline-table;
+  width: 100%;
+  max-height: 318px;
+  /*margin-left: 10%;
     margin-right: 10%;*/
 }
 
-tr,th,td {
-    border:1px solid black;
-    font-size:15px;
-    min-height: 40px;
-    max-width: 280px; 
+tr,
+th,
+td {
+  border: 1px solid black;
+  font-size: 15px;
+  min-height: 40px;
+  max-width: 280px;
 }
 
 #celdaEliminar {
-    max-width: 20px;
+  max-width: 20px;
 }
 
 #btnEliminar {
-    cursor:pointer;
-    margin-left: 40%;
+  cursor: pointer;
+  margin-left: 40%;
 }
 </style>
 
@@ -277,46 +299,106 @@ import MainHead from "@/components/MainHead.vue";
 import Menu from "@/components/Menu.vue";
 import Footer from "@/components/Footer.vue";
 
-
 export default {
-    name: "ModificacionEstudiantes",
-    components: {
-        MainHead,
-        Menu,
-        Footer,
-    },
-    data() {
-        return {
-            items : [
-                {id:"1",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
-                {id:"2",nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
-                {id:"3",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
-                {id:"4",nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
-                {id:"5",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
-                {id:"6",nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
-                {id:"7",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
-                {id:"8",nombre: 'Carmen Valenzuela Bernal',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
-                {id:"9",nombre: 'Oscar Campos Ardila', identificacion:'234234', situacion:'Matriculado', grado:'6°-1'},
-                {id:"10",nombre: 'Carmen Valenzuela Bernal ',identificacion:'234235',situacion:'Matriculado',grado:'6°-1'},
-                ],
-            buscador: '', //Estos valores se usará de filtro
-            grado:'',
-            curso:'',
-        }
-    },
-    methods: {
-        eliminar(row) {
-            if (confirm("¿Está seguro de eliminar?")){
-                this.items.splice(row,1);}
+  name: "ModificacionEstudiantes",
+  components: {
+    MainHead,
+    Menu,
+    Footer,
+  },
+  data() {
+    return {
+      items: [
+        {
+          id: "1",
+          nombre: "Oscar Campos Ardila",
+          identificacion: "234234",
+          situacion: "Matriculado",
+          grado: "6°-1",
         },
-        añadir() {
-            this.items.push({
-                nombre:'',
-                identificacion:'',
-                situacion:'',
-                grado:'',
-            })
+        {
+          id: "2",
+          nombre: "Carmen Valenzuela Bernal",
+          identificacion: "234235",
+          situacion: "Matriculado",
+          grado: "6°-1",
         },
-    }
-}
+        {
+          id: "3",
+          nombre: "Oscar Campos Ardila",
+          identificacion: "234234",
+          situacion: "Matriculado",
+          grado: "6°-1",
+        },
+        {
+          id: "4",
+          nombre: "Carmen Valenzuela Bernal",
+          identificacion: "234235",
+          situacion: "Matriculado",
+          grado: "6°-1",
+        },
+        {
+          id: "5",
+          nombre: "Oscar Campos Ardila",
+          identificacion: "234234",
+          situacion: "Matriculado",
+          grado: "6°-1",
+        },
+        {
+          id: "6",
+          nombre: "Carmen Valenzuela Bernal",
+          identificacion: "234235",
+          situacion: "Matriculado",
+          grado: "6°-1",
+        },
+        {
+          id: "7",
+          nombre: "Oscar Campos Ardila",
+          identificacion: "234234",
+          situacion: "Matriculado",
+          grado: "6°-1",
+        },
+        {
+          id: "8",
+          nombre: "Carmen Valenzuela Bernal",
+          identificacion: "234235",
+          situacion: "Matriculado",
+          grado: "6°-1",
+        },
+        {
+          id: "9",
+          nombre: "Oscar Campos Ardila",
+          identificacion: "234234",
+          situacion: "Matriculado",
+          grado: "6°-1",
+        },
+        {
+          id: "10",
+          nombre: "Carmen Valenzuela Bernal ",
+          identificacion: "234235",
+          situacion: "Matriculado",
+          grado: "6°-1",
+        },
+      ],
+      buscador: "", //Estos valores se usará de filtro
+      grado: "",
+      curso: "",
+    };
+  },
+  methods: {
+    eliminar(row) {
+      if (confirm("¿Está seguro de eliminar?")) {
+        this.items.splice(row, 1);
+      }
+    },
+    añadir() {
+      this.items.push({
+        nombre: "",
+        identificacion: "",
+        situacion: "",
+        grado: "",
+      });
+    },
+  },
+};
 </script>
